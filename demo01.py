5156cbb618e20f97f46ce302ceaa915d617eec13
Roger <danyour@outlook.com>
2023-12-28 10:48:19 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,11 @@
+print("hello world")  # 字符串
+print(2333)  # 整数
+print(2.33)  # 小数
+print(False) # 布尔值
+print(())  # 元组
+print([])  # 数组
+print({})  # 字典
+
+print("哈哈",2333,2.333)
+print("哈哈"+"嘻嘻")  # 字符串的拼接
+print("啊哈"*10)
